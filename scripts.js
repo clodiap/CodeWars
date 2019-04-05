40e3653fc6e2c959839dac216671f4492920491f
Clodiap <clodiapp@gmail.com>
2019-04-05 18:47:32 +0200

    javascript exercise

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// Very simple, given a number, find its opposite.
+
+function opposite(number) {
+  return - number
+}
+console.log(opposite(9))
+/* Autres solutions
+
+const opposite = number => -number;
+
+function opposite(number) {
+    return number * (-1);
+}
+
+*/

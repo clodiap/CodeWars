465796585a9a6ed4ea4c8965a5906b0a16d7eaed
Clodiap <clodiapp@gmail.com>
2019-06-11 09:49:18 +0200

    ruby exercises + tests ok

---
+++ spec/mumbling_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/mumbling'
+
+describe "accum" do
+  it "returns" do
+    expect(accum("ZpglnRxqenU")).to eq("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+    expect(accum("NyffsGeyylB")).to eq("N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+    expect(accum("MjtkuBovqrU")).to eq("M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+    expect(accum("EvidjUnokmM")).to eq("E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+    expect(accum("HbideVbxncC")).to eq("H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+
+  end
+end
